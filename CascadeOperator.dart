318f2c8e6872c0c77eb978a3f684681df4e184f9
randjith4ever <randjithkumar@gmail.com>
2019-11-26 19:33:04 +0800

    demonstration on int to String, dart fat arrow in methods&Cascade operator execu sequential function

---
+++ CascadeOperator.dart
@@ -0,0 +1,27 @@
+class CascadeOperator {
+  void test_method() {
+    int num = 50;
+    print(
+        "Here the int type can be Stringified using toString method: ${num.toString()}");
+  }
+
+  void test_method1() {
+    print("This is a  test method1");
+  }
+
+  void test_method2() => print(
+      "This is a test method2"); //Used to print single line statements primarily setters-getters
+
+  void test_method3() {
+    print("This is a  test method3");
+  }
+}
+
+void main() {
+  new CascadeOperator()
+    ..test_method()
+    ..test_method1()
+    ..test_method1()
+    ..test_method2()
+    ..test_method3();
+}
